2377aee96e831235c0f6913fc20976bdad69dfc9
Sayreks <v.bezgubovv1@gmail.com>
2024-03-14 01:04:49 +0500

    dz winda

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+    Image = models.ImageField(upload_to='preview/', verbose_name='Изображение', **NULLABLE)
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    purchase_price = models.CharField(max_length=100, verbose_name='Цена за покупку')
+    created_at = models.CharField(max_length=100, verbose_name='Дата создания')
+    updated_at = models.CharField(max_length=100, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('name',)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('name',)
