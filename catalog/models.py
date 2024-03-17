fdaadd2f470d64d512a3c41c6e53904fa1e4d101
saureks <v.bezgubovv1@gmail.com>
2024-03-18 01:41:33 +0500

    migrate 1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+    Image = models.ImageField(upload_to='preview/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    purchase_price = models.IntegerField(max_length=100, verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(max_length=100, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(max_length=100, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('name',)
