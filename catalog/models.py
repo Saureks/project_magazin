ba2e2ac2d8f9b251af4eeda2748b6bf56f4d3647
saureks <v.bezgubovv1@gmail.com>
2024-03-21 20:29:41 +0500

    Fix

--- catalog/models.py
+++ catalog/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     description = models.CharField(max_length=100, verbose_name='Описание')
     Image = models.ImageField(upload_to='preview/', verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
-    purchase_price = models.IntegerField(max_length=100, verbose_name='Цена за покупку')
+    purchase_price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateTimeField(max_length=100, verbose_name='Дата создания')
     updated_at = models.DateTimeField(max_length=100, verbose_name='Дата последнего изменения')
 
