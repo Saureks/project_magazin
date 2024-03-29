1e999d4770d5383888203b624f11b7c433237a4b
saureks <v.bezgubovv1@gmail.com>
2024-03-29 23:28:56 +0500

    6.0

--- catalog/models.py
+++ catalog/models.py
@@ -1,35 +1,36 @@
 from django.db import models
 
-NULLABLE = {'blank': True, 'null': True}
+# Create your models here.
+
+NULLABLE = {"null": True, "blank": True}
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.CharField(max_length=100, verbose_name='Описание')
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(**NULLABLE, verbose_name="Описание")
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return f"{self.name}"
 
     class Meta:
-        verbose_name = 'товар'
-        verbose_name_plural = 'товары'
-        ordering = ('name',)
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.CharField(max_length=100, verbose_name='Описание')
-    Image = models.ImageField(upload_to='preview/', verbose_name='Изображение', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
-    purchase_price = models.IntegerField(verbose_name='Цена за покупку')
-    created_at = models.DateTimeField(max_length=100, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(max_length=100, verbose_name='Дата последнего изменения')
-    manufactured_at = models.DateTimeField(max_length=100, verbose_name='Дата производства продукта')
+    name = models.CharField(max_length=250, verbose_name="Наименование")
+    description = models.TextField(**NULLABLE, verbose_name="Описание")
+    image = models.ImageField(upload_to="catalog/", **NULLABLE, verbose_name="Превью")
+    # category = models.CharField(max_length=100, verbose_name="Категория")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.IntegerField(verbose_name="Цена за покупку")
+    created_at = models.DateField(**NULLABLE, verbose_name="Дата создания")
+    updated_at = models.DateField(**NULLABLE, verbose_name="Дата последнего изменения")
+    # manufactured_at = models.DateField(**NULLABLE, verbose_name="Дата производства продукта")
 
     def __str__(self):
-        return f'{self.name} {self.description}'
+        return f"{self.name} ({self.category}) - {self.price}"
 
     class Meta:
-        verbose_name = 'товар'
-        verbose_name_plural = 'товары'
-        ordering = ('name',)
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
